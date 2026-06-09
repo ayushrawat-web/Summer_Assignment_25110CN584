4e8659c2ee1bfa0a80db63d7f99560717b7b88f3
ayush singh rawat <ayushnda2@gmail.com>
2026-06-09 13:30:42 +0530

    Day 9 uploaded

---
+++ Day-9/33_reverse_star_pattern.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    printf("enter no of rows:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=5;j>=i;j--)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
